e6360fb961bdd06d663bcf0f4c655976fa75662e
Sumit Maurya <smithdashdash@gmail.com>
2024-03-02 15:06:49 +0530

    design fix

--- components/chatComponent.tsx
+++ components/chatComponent.tsx
@@ -9,10 +9,10 @@ export default function ChatComponent() {
 
     return (
 <div className="chat-box flex flex-col h-screen overflow-y-auto bg-gray-100 dark:bg-gray-800 px-4 py-8 rounded-md shadow-md">
-      {/* Message history */}
-      <h3 className="assistant-name text-lg font-semibold text-blue-700">
-                  Travel Thousand Miles AI Bot
-                </h3>
+      
+    <h3 className="assistant-name text-lg font-semibold text-blue-700 flex ">
+        Travel Thousand Miles AI Bot
+    </h3>
       <div className="message-history flex flex-col gap-4 overflow-y-auto">
         {messages.map((message: Message) => (
           <div
@@ -29,7 +29,7 @@ export default function ChatComponent() {
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  {/* Replace with your preferred assistant icon */}
+                
                   <path
                     d="M9 5H7C4.8 5 3 6.8 3 9v12c0 2.2 1.8 4 4 4h2a2 2 0 0 0 2-2V9zM15 5h-2C12.2 5 11 6.8 11 9v12c0 2.2 1.8 4 4 4h2a2 2 0 0 0 2-2V9z"
                     fill="currentColor"
@@ -48,7 +48,7 @@ export default function ChatComponent() {
                 </h3>
               ) : (
                 <h3 className="user-name text-lg font-semibold text-gray-700 dark:text-gray-200">
-                  User
+                  You
                 </h3>
               )}
               <div className="message-text text-gray-600 dark:text-gray-200 text-base">
@@ -59,11 +59,11 @@ export default function ChatComponent() {
         ))}
       </div>
 
-      {/* User input form */}
+      
       <form className="mt-8 flex items-center justify-between w-full" onSubmit={handleSubmit}>
         <textarea
-          className="chat-input flex-grow bg-gray-100 dark:bg-gray-800 border border-transparent rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 resize-none"
-          placeholder="Ask your question..."
+          className="chat-input flex-grow bg-gray-100 dark:bg-gray-800 border  rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 border:ring-blue-500 focus:ring-opacity-50 resize-none"
+          placeholder="Planning your next trip? Let's chat..."
           value={input}
           onChange={handleInputChange}
         />
